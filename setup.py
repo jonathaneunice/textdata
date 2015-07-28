2ac2ba5a96f534107aa93365d0d0e832ae1f92a2
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-07-28 10:12:41 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='textdata',
-    version='1.1.0',
+    version='1.1.1',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Get clean line or text data from multi-line strings',
