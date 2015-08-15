cdc7967bd0de0a3d15749852d09d9732d766e548
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-15 18:24:12 -0400

    1.3.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def lines(text):
 
 setup(
     name='textdata',
-    version='1.2.3',
+    version='1.3.0',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Easily get clean data, direct from Python source',
